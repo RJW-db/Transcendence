f12bbae5e14cab232be9b87e38babc067e1ac825
sam desktop <saleunin@student.codam.nl>
2025-11-03 22:44:37 +0100

    started testoauth serv

---
+++ routes/auth-routes.js
@@ -0,0 +1,25 @@
+const { Router } = require('express');
+
+const router = require('express').Router();
+
+// auth login
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// auth logout
+router.get('/logout', (req, res) => {
+    //handle with passport
+
+});
+
+
+// auth with google
+
+router.get("/google", (req, res) => {
+    //handle with passport
+    res.send('logging in with google');
+});
+
+module.exports = router;
