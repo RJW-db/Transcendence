5889f2a705a733f9cf4a9a640f34ba0297f28c65
saleunin <saleunin@student.codam.nl>
2025-11-07 15:21:47 +0100

    added check for if user exists

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -8,6 +8,15 @@ const prisma = new PrismaClient();
 // accesstoken token given for getting info
 // refreshToken get a new access token without the user's interaction.
 
+// passport.serializeUser((user, done) => {
+//     done(null, user.ID);
+// });
+
+// passport.deserializeUser((id, done) => {
+//     User.findById(id).then((user) => {
+//         done(null, user);
+//     });
+// });
 
 passport.use(
     new GoogleStrategy({
@@ -18,20 +27,31 @@ passport.use(
     }, async (accessToken, refreshToken, profile, done) => {
         // passport callback function
         console.log('passport callback function fired');
-        console.log(profile);
-        console.log(profile.emails[0].value);
-
-        // console.log(profile.em)
-        const user = await prisma.user.create({
-            data: {
-            Alias: profile.displayName,
-			Email: profile.emails[0].value,
-			Password: 'password123',
-			Online: true,
-			CreationDate: new Date(),
-            }
+        
+        let user = await prisma.user.findUnique({
+            where: { Email: profile.emails[0].value }
         });
-	    console.log('Created User:', user);
+        if (!user) {
+            user = await prisma.user.create({
+                data: {
+                    Alias: profile.displayName,
+                    Email: profile.emails[0].value,
+                    Password: 'password123',
+                    Online: true,
+                    CreationDate: new Date(),
+                }
+            });
+            console.log('Created User:', user);
+            done(null, user);
+        }
+        else
+        {
+            console.log('user is: ', user);
+            done(null, user);
+        }
+            
+        // console.log(profile.em)
+
         // done();
     })
 );
